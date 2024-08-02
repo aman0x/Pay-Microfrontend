5fab787ef06ef88f71131f2d697f282007e95b80
aman0x <amanchandel4@gmail.com>
2024-08-02 20:17:10 +0530

    UI fixes: Responsive Error page

--- AppShell/src/components/Error/index.js
+++ AppShell/src/components/Error/index.js
@@ -4,13 +4,13 @@ import {useNavigate} from "react-router-dom"
 const NotFound = () => {
     const navigate = useNavigate()
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100 w-full">
-      <h1 className="text-3xl font-bold mb-4">Sorry for the issue!</h1>
+    <div className="p-5 flex flex-col items-center justify-center my-20 md:my-0 h-full bg-gray-100 w-full">
+      <h1 className=" text-3xl font-bold mb-4">Sorry for the issue!</h1>
       <p className="text-lg mb-6 text-center">
         The page you are looking has some issues, but don't worry, you can access other services easily.
       </p>
       <p className="font-bold my-2">Go To</p>
-      <div  className="flex gap-2">
+      <div  className="flex flex-wrap gap-2">
         <button
         className="border-2 py-2 px-4 rounded-2xl bg-black-primary primary-btn"
         onClick={()=>
